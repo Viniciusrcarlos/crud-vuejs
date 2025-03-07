66d6bd9c99e49f17ae95ed31b43104f0a5b5ba92
Vinicius Raupp <101226503+Viniciusrcarlos@users.noreply.github.com>
2025-03-06 22:37:50 -0300

    daisyui

--- src/App.vue
+++ src/App.vue
@@ -28,18 +28,35 @@ const addUser = () => {
 
 <template>
   <Header />
-  <ul>
-    <li v-for="user in userStore.users">
-      {{ user.name }}
-      <button @click="userStore.deleteUsers(user.id)">DELETE</button>
-    </li>
-  </ul>
+  <div class="flex flex-col items-center justify-center min-h-screen">
+    <div class="w-80 flex flex-col items-center gap-6 bg-base-200 rounded-lg shadow-lg p-6">
 
-  <h2>Add User</h2>
-  <input v-model="name" placeholder="Name" />
-  <input v-model="email" placeholder="Email" />
-  <input v-model="phone" placeholder="Phone" />
-  <button @click="addUser">ADD</button>
+      <!-- Lista de Usuários -->
+      <ul class="w-full bg-base-100 rounded-lg shadow p-4">
+        <li
+            v-for="user in userStore.users"
+            :key="user.id"
+            class="flex justify-between items-center p-2 border-b last:border-none"
+        >
+          <span class="text-lg">{{ user.name }}</span>
+          <button class="btn btn-error btn-sm" @click="userStore.deleteUsers(user.id)">DELETE</button>
+        </li>
+      </ul>
+
+      <!-- Formulário de Adição -->
+      <div class="w-full flex flex-col items-center gap-4">
+        <h2 class="text-xl font-bold">Add User</h2>
+        <input class="input input-bordered w-full" v-model="name" placeholder="Name" />
+        <input class="input input-bordered w-full" v-model="email" placeholder="Email" />
+        <input class="input input-bordered w-full" v-model="phone" placeholder="Phone" />
+        <button class="btn btn-primary w-full" @click="addUser">ADD</button>
+      </div>
+
+    </div>
+  </div>
+
+
+
 
 </template>
 
